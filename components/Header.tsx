910c4c38ee6cb66e45bb36c128370824208a9176
Loli250 <MTV888@protonmail.com>
2026-08-05 09:24:33 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         <div>
           <a href="https://www.youtube.com/@AM_CLUB" target="_blank">
             <Text size="xl" span>
-              🕒
+              🎉
             </Text>
             <Text
               size="xl"
@@ -32,7 +32,7 @@ export default function Header() {
               variant="gradient"
               gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
             >
-              服务监控
+              Service Monitoring
             </Text>
           </a>
         </div>
